fd21f866fa71810fd8b42f8ea3e6a63548016b38
SirCrocodile1010 <bijitbrahma1010@gmail.com>
2026-06-23 09:56:01 +0530

    Update stream source

--- providers/zstream.js
+++ providers/zstream.js
@@ -1,6 +1,6 @@
 /**
  * zstream - Built from src/zstream/
- * Generated: 2026-06-23T03:46:46.253Z
+ * Generated: 2026-06-23T04:26:01.022Z
  */
 var __async = (__this, __arguments, generator) => {
   return new Promise((resolve, reject) => {
@@ -24,68 +24,26 @@ var __async = (__this, __arguments, generator) => {
 };
 
 // src/zstream/index.js
-var BASE_VIDSRC = "https://vidsrc.to/embed";
-var BASE_AUTOEMBED = "https://autoembed.cc/api/tm";
-function fetchVidsrc(tmdbId, mediaType, season, episode) {
-  return __async(this, null, function* () {
-    const streams = [];
-    try {
-      let url;
-      if (mediaType === "movie") {
-        url = `${BASE_VIDSRC}/movie/${tmdbId}`;
-      } else {
-        url = `${BASE_VIDSRC}/tv/${tmdbId}/${season}/${episode}`;
-      }
-      const res = yield fetch(url);
-      const html = yield res.text();
-      const srcMatches = html.match(/src:\s*["']([^"']+m3u8[^"']*)/g);
-      if (srcMatches) {
-        srcMatches.forEach((match, i) => {
-          const u = match.replace(/src:\s*["']/, "");
-          streams.push({ name: "ZStream", title: `VidSrc ${i + 1}`, url: u, quality: "HD" });
-        });
-      }
-    } catch (e) {
-      console.log("VidSrc error:", e.message);
-    }
-    return streams;
-  });
-}
-function fetchAutoembed(tmdbId, mediaType, season, episode) {
-  return __async(this, null, function* () {
-    const streams = [];
-    try {
-      let url;
-      if (mediaType === "movie") {
-        url = `${BASE_AUTOEMBED}/movie/${tmdbId}`;
-      } else {
-        url = `${BASE_AUTOEMBED}/tv/${tmdbId}/${season}/${episode}`;
-      }
-      const res = yield fetch(url);
-      const data = yield res.json();
-      if (data && data.url) {
-        streams.push({ name: "ZStream", title: "AutoEmbed HD", url: data.url, quality: "HD" });
-      }
-      if (data && data.streams) {
-        data.streams.forEach((s, i) => {
-          streams.push({ name: "ZStream", title: `AutoEmbed ${i + 1}`, url: s.url || s, quality: s.quality || "HD" });
-        });
-      }
-    } catch (e) {
-      console.log("AutoEmbed error:", e.message);
-    }
-    return streams;
-  });
-}
 function getStreams(tmdbId, mediaType, season, episode) {
   return __async(this, null, function* () {
     const streams = [];
-    const [vidsrcStreams, autoembedStreams] = yield Promise.all([
-      fetchVidsrc(tmdbId, mediaType, season, episode),
-      fetchAutoembed(tmdbId, mediaType, season, episode)
-    ]);
-    streams.push(...vidsrcStreams);
-    streams.push(...autoembedStreams);
+    try {
+      let url;
+      if (mediaType === "movie") {
+        url = `https://vidlink.pro/movie/${tmdbId}`;
+      } else {
+        url = `https://vidlink.pro/tv/${tmdbId}/${season}/${episode}`;
+      }
+      streams.push({
+        name: "ZStream",
+        title: "VidLink HD",
+        url,
+        quality: "HD",
+        type: "embed"
+      });
+    } catch (e) {
+      console.log("Error:", e.message);
+    }
     return streams;
   });
 }
