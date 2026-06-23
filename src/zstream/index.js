783541bc447652e870c9586f7187d1d44b9213a0
SirCrocodile1010 <bijitbrahma1010@gmail.com>
2026-06-23 13:33:16 +0530

    Fix stream source vidsrc

--- src/zstream/index.js
+++ src/zstream/index.js
@@ -1,28 +1,22 @@
-var BASE_URL = "https://4khdhub.click";
-var TMDB_KEY = "439c478a771f35c05022f9feabcca01c";
-var UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36";
-
 async function getStreams(tmdbId, mediaType, season, episode) {
   const streams = [];
   try {
-    const headers = { "User-Agent": UA };
-    const tmdbUrl = mediaType === "movie"
-      ? "https://api.themoviedb.org/3/movie/" + tmdbId + "?api_key=" + TMDB_KEY
-      : "https://api.themoviedb.org/3/tv/" + tmdbId + "?api_key=" + TMDB_KEY;
-    const tmdbRes = await fetch(tmdbUrl, { headers });
-    const tmdbData = await tmdbRes.json();
-    const title = tmdbData.title || tmdbData.name || "";
-    const searchUrl = BASE_URL + "/search?q=" + encodeURIComponent(title);
-    const searchRes = await fetch(searchUrl, { headers });
-    const searchHtml = await searchRes.text();
-    const m3u8Links = searchHtml.match(/https?:\/\/[^\s"']+\.m3u8[^\s"']*/g);
-    if (m3u8Links) {
-      m3u8Links.forEach(function(link, i) {
-        streams.push({ name: "ZStream", title: "4K Stream " + (i+1), url: link, quality: "4K" });
+    var apiUrl;
+    if (mediaType === 'movie') {
+      apiUrl = 'https://vidsrc.me/embed/movie?tmdb=' + tmdbId;
+    } else {
+      apiUrl = 'https://vidsrc.me/embed/tv?tmdb=' + tmdbId + '&season=' + season + '&episode=' + episode;
+    }
+    var res = await fetch(apiUrl, { headers: { 'User-Agent': 'Mozilla/5.0' } });
+    var html = await res.text();
+    var links = html.match(/https?://[^s"'<>]+.(m3u8|mp4)[^s"'<>]*/g);
+    if (links) {
+      links.forEach(function(link, i) {
+        streams.push({ name: 'ZStream', title: 'Stream ' + (i+1), url: link, quality: 'HD' });
       });
     }
-  } catch (e) {
-    console.log("Error:", e.message);
+  } catch(e) {
+    console.log('Error:', e.message);
   }
   return streams;
 }
